6e5ce09af04feeb2cdd09bdb251487f61af8d41b
Perush Parajuli <perushparajuli@gmail.com>
2025-04-02 22:07:49 +0545

    Initial route creation

---
+++ server/routes/Url.js
@@ -0,0 +1,3 @@
+const urlModel = require("../model/Url")
+const express = require("express")
+const router = express.Router()
---
+++ server/routes/User.js
@@ -0,0 +1,3 @@
+const userModel = require("../model/User")
+const express = require(express)
+const router = express.Router()
