41a054f1d1437d474a8463c9a1ca2b991e89e887
Perush Parajuli <perushparajuli@gmail.com>
2025-04-24 22:49:04 +0545

    Created Skeleton for Url shortener. Here user can enter their Original URL to get their shortened URL

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import SignUp from "./assets/pages/SignUp";
 import SignIn from "./assets/pages/SignIn";
 import { Routes, Route } from "react-router-dom";
 import SignUpConfirmation from "./assets/pages/SignUpConfirmation";
+import Home from "./assets/pages/Home";
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
         <Route path="signup" element={<SignUp />} />
         <Route path="signup/confirmation" element={<SignUpConfirmation />} />
         <Route path="signin" element={<SignIn />} />
+        <Route path="home" element={<Home />}/>
       </Routes>
     </div>
   );
